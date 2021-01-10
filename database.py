d4485ba587c2d3a0ad55e4a569c02cb0828a6bdf
ECiccotelli <ericciccotelli@gmail.com>
2021-01-10 18:04:13 -0500

    Database class to connect, insert and query the database and information

--- database.py
+++ database.py
@@ -4,18 +4,9 @@ import mysql.connector
 class Database:
 
     def __init__(self):
-        self.readCreds()
-        print("Init")
-    
-    #Inserts data into database
-    def insertData(self, mycursor, conn, apikey, name, dateIssued, email):
-        sql = "INSERT INTO apikeystable VALUES (%s, %s, %s, %s)"
-        args = apikey, name, dateIssued, email
-        mycursor.execute(sql, args)
-        #mycursor.execute(statement)
-        conn.commit()
-    
-    
+        self.readCreds() #Reads information into file to connect to database
+
+    #Reading database credentials from file
     def readCreds(self):
         with open('dbcreds.json') as f:
             data = json.load(f)
@@ -25,9 +16,9 @@ class Database:
         self.host = data['host']
         self.databaseName = data['database']
         self.port = data['port']
-        print(self.databaseName)
 
-        
+    
+    #Connects to database
     def connectToDatabase(self):
         conn = mysql.connector.connect(user=self.username, password=self.password,
                                 host=self.host,
@@ -39,15 +30,28 @@ class Database:
         else:
             print("Error connecting to database")
         
+        #Selects proper database and table
         mycursor.execute("use apikeys")
-        mycursor.execute("Select * from apikeystable;")
-        data = mycursor.fetchall()
-        print("Data before!")
-        print(data)
+        self.conn = conn
+        self.cursor = mycursor
+        return conn, mycursor
+    
+    #Inserts data into database
+    def insertData(self, mycursor, conn, apikey, name, dateIssued, email, ID):
+        sql = "INSERT INTO apikeystable VALUES (%s, %s, %s, %s, %s)"
+        args = apikey, name, dateIssued, email, ID
+        mycursor.execute(sql, args)
+        conn.commit()
+    
+    #Queries database to return information about a given ID
+    def queryDB(self, ID):
 
-        insertData(mycursor, conn, hashKey(), getName(), getDate(), getEmail())
-
-        mycursor.execute("Select * from apikeystable;")
-        data = mycursor.fetchall()
-        print("Data after!")
-        print(data)
+        self.cursor.execute("Select * from apikeystable where ID = '" + str(ID) + "';")
+        data = self.cursor.fetchall()
+        return data
+    
+    def getConn(self):
+        return self.conn
+    
+    def getCursor(self):
+        return self.cursor
